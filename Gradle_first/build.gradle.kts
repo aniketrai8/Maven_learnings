eddc4120b5659d1edb8ef0f4f0f45a570521efb6
aniketrai8 <rai.aniket@outlook.com>
2025-09-17 17:24:08 +0530

    Adding my Gradle Project

---
+++ Gradle_first/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Logging
+    implementation("org.slf4j:slf4j-api:2.0.11")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+
+    // Testing
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+
+tasks.test {
+    useJUnitPlatform() // Enables JUnit 5
+}
---
+++ Gradle_first/src/test/java/com/example/AppTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+
+public class AppTest
+{
+   void TestMsg(){
+       String msg="Hello, Gradle basic setup";
+       assertEquals("Hello, Gradle basic setup",msg);
+   }
+}
