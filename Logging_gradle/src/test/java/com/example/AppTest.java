bd932e5d60f64aab7c50dd0c2f6601ffb06c7907
aniketrai8 <rai.aniket@outlook.com>
2025-09-22 11:36:35 +0530

    Gradle logging dependency

---
+++ Logging_gradle/src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    void TestMultiply(){
+        assertEquals(6*7,42);
+    }
+}
